21ecb8545be9c99b00a223fdb110b92abb39eed7
Alex Berber <berber.it@gmail.com>
2016-07-09 15:23:34 +0300

    headers added, urllib logic improved

--- utils/utils.py
+++ utils/utils.py
@@ -26,9 +26,17 @@ class Utils(object):
 
     @staticmethod
     def checkIfUrlAlive(cleanURL):
+        hdr = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+            'Accept-Encoding': 'none',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Connection': 'keep-alive'}
         try:
-            response = urllib2.urlopen(cleanURL)
-            statusCode = response.getcode()
+            response = urllib2.Request(cleanURL, headers=hdr)
+            page = urllib2.urlopen(response)
+            statusCode = page.getcode()
             if str(statusCode) == '200':
                 print('Web site exists')
                 print('Web site response code: %s' % statusCode)
